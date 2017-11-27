1e4c835157e9afea19ddf69736ec7efcaa9088f0
Eduard Badillo <eduard.badillo@gmail.com>
2017-11-27 01:50:08 +0100

    Add vue url

--- hellovue/urls.py
+++ hellovue/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from vue.views import vue
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^vue', vue)
 ]
