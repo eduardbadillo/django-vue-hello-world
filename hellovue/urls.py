cf469f9760bdb8dd771e81dd637a8c1feac546ae
Eduard Badillo <eduard.badillo@gmail.com>
2017-12-03 20:29:53 +0100

    Add example for rest_framework

--- hellovue/urls.py
+++ hellovue/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from vue.views import vue
+from rest_framework import routers
+from vue.views import vue, UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
+    url(r'^$', vue),
     url(r'^admin/', admin.site.urls),
-    url(r'^vue', vue)
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
